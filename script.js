f77e73dc6f78b0ca9223a9ff9d5086b60c45073c
Nethenjohn <138705169+Nethenjohn@users.noreply.github.com>
2023-07-06 17:36:29 +0100

    Updated page
    Redirect to page when clicking on card. each type has a corresponding colour. need to implement working carousel and create dynamic webpage for every pokemon

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function fetchPokemon(searchString) {
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=2000&') // Fetching all 898 Pokemon
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=2000') // Fetching all 898 Pokemon
   .then(response => response.json())
   .then(data => {
+    console.log(data)
     document.getElementById('cardContainer').innerHTML = 'Loading...';
+
     const pokemonList = data
       .results
       .filter(pokemon => {
@@ -10,9 +12,8 @@ function fetchPokemon(searchString) {
           ? pokemon.name.includes(searchString)
           : true;
       })
-      .slice(0, 16); // Limiting to the first 12 Pokemon for initial display
-      let currentPage = 0;
-      const cardsPerPage = 16;
+      .slice(0, 32); // Limiting to the first 32 Pokemon for initial display
+     
 
     document.getElementById("cardContainer").innerHTML = "";
     // Fetch and store details of each Pokemon
@@ -31,16 +32,20 @@ function fetchPokemon(searchString) {
       // Extracting the relevant information
       const pokemonName = name;
       const pokemonImage = sprites.front_default;
-      const pokemonTypes = types.map(type => type.type.name);
+      const pokemonTypes = pokemonData.types.map(type => {
+        return `<li style="background-color: ${typeColors[type.type.name] || '#000'}">${type.type.name}</li>`;
+      });
 
+      
+      
       // Generate HTML for Pokemon card
       const cardHTML = `
-        <div class="card">
-          <h2>${pokemonName}</h2>
-          <span><img src="${pokemonImage}" alt="${pokemonName}"></span>
-          <span><p>${pokemonTypes.join(', ')}</p></span>
-        </div>
-      `;
+      <div class="card" onclick="redirectToPokemon('${pokemonName}')">
+       <span><img src="${pokemonImage}" alt="${pokemonName}"></span>
+       <h2>${pokemonName}</h2>
+       <ul class="types">${pokemonTypes.join('')}</ul>
+      </div>
+  `;
 
       // Append the card to the cardContainer div
       document.getElementById('cardContainer').innerHTML += cardHTML;
@@ -52,6 +57,19 @@ function fetchPokemon(searchString) {
   .catch(error => console.log(error));
 }
 
+function createPokemonPage(pokemonName, pokemonData) {
+  // Use the Pokémon data to dynamically generate the content of the individual webpage
+  // You can customize the webpage layout and include relevant details about the Pokémon
+  // Save the generated content to a new HTML file with the Pokémon name as the filename
+  // Repeat this process for each Pokémon in the list
+}
+
+
+function redirectToPokemon(pokemonName) {
+  // Redirect to the webpage specifically about the Pokémon
+  // Replace "pokemonpage.html" with the desired URL or file name
+  window.location.href = `pokemonpage.html?name=${pokemonName}`;
+}
 
 // Search for a specific Pokemon
 function searchPokemon() {
@@ -60,5 +78,32 @@ function searchPokemon() {
  fetchPokemon(searchInput);
 }
 
+const typeColors = {
+  normal: '#A8A77A',
+  fire: '#EE8130',
+  water: '#6390F0',
+  electric: '#F7D02C',
+  grass: '#7AC74C',
+  ice: '#96D9D6',
+  fighting: '#C22E28',
+  poison: '#A33EA1',
+  ground: '#E2BF65',
+  flying: '#A98FF3',
+  psychic: '#F95587',
+  bug: '#A6B91A',
+  rock: '#B6A136',
+  ghost: '#735797',
+  dragon: '#6F35FC',
+  dark: '#705746',
+  steel: '#B7B7CE',
+  fairy: '#D685AD',
+  // Add more Pokémon types and corresponding colors as needed
+};
+
+function redirectToPokemon(pokemonName) {
+  window.location.href = `pokemon/${pokemonName}.html`;
+}
+
 
 fetchPokemon();
+
